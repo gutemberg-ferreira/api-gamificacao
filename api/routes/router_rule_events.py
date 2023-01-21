575eb781da630e490791650c2514f53a5b4762df
Gutemberg Ferreira <gutemf@motorola.com>
2023-01-21 18:59:55 -0400

    Rota de Eventos e Testes

--- api/routes/router_rule_events.py
+++ api/routes/router_rule_events.py
@@ -1,21 +1,9 @@
-from flask import Blueprint, jsonify, request
+from flask import jsonify, request
 from flasgger import swag_from
-router_rule_events= Blueprint(name="_ruler_events", import_name=__name__)
+from app import app
 
-@router_rule_events.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 @swag_from('../../api_docs/Rule_Events/Get_All_Rule_Events.yml')
-def test():
+def get_all_events():
     output = {"msg": "I'm the router_test endpoint from blueprint_x."}
-    return jsonify(output)
-
-# add view function to the blueprint
-@router_rule_events.route('/plus', methods=['POST'])
-#@swag_from('../../api_docs/Router_Test_Post_Sun.yml')
-def plus_x():
-    # retrieve body data from input JSON
-    data = request.get_json()
-    in_val = data['number']
-    # compute result and output as JSON
-    result = in_val + 5
-    output = {"msg": f"Your result is: '{result}'"}
     return jsonify(output)
--- api/routes/router_test.py
+++ api/routes/router_test.py
@@ -1,25 +1,23 @@
-from flask import Blueprint, jsonify, request
+from flask import jsonify, request
 from flasgger import swag_from
-router_test= Blueprint(name="router_test", import_name=__name__)
-import api_docs
-
+from app import app
 # note: global variables can be accessed from view functions
 x = 5
 
 # add view function to the blueprint
-@router_test.route("/")
+@app.route("/")
 @swag_from('../../api_docs/Tests/Router_Test_Get_Root.yml')
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@router_test.route('/test', methods=['GET'])
+@app.route('/test', methods=['GET'])
 @swag_from('../../api_docs/Tests/Router_Test_Get_Test.yml')
 def test():
     output = {"msg": "I'm the router_test endpoint from blueprint_x."}
     return jsonify(output)
 
 # add view function to the blueprint
-@router_test.route('/plus', methods=['POST'])
+@app.route('/plus', methods=['POST'])
 @swag_from('../../api_docs/Tests/Router_Test_Post_Sun.yml')
 def plus_x():
     # retrieve body data from input JSON
